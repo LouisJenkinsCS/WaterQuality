aabb8d105fc0977e730bb68dcd7b9c14201a9927
bsm29471 <bsm29471@BFH115-24.buad.bloomu.edu>
2017-03-20 21:52:00 -0400

    Front-end admin deletion is finished (pending Louis' approval), documented for clarity; commented servlet for ease of understanding necessary steps

--- web/scripts/admin_deletion.js
+++ web/scripts/admin_deletion.js
@@ -5,32 +5,32 @@
 
 $.getScript("scripts/AJAX_magic.js", function () {});
 
-var deleteRequest = {action: 'RemoveData',
-    dataName: 'TestData',
-    startTime: '2007-12-03T10:15:30',
-    endTime: '2017-03-19T09:15:00'};
-
+//del_options will hold the retrieved data names from
+//the table ManualDataNames
 var del_options = "";
+
+//Defines how AdminServlet responds
 var dataRequest = {action: 'getManualItems'};
 
+//Called in admin.jsp to load this script
 function deleteTheStuff()
 {
     get("AdminServlet", dataRequest, function (response)
     {
         console.log(response);
         console.log("Connection made!" + response);
-        var thing = JSON.parse(response)["data"];
-        for (var i = 0; i < thing.length; i++)
+        var param_names = JSON.parse(response)["data"];
+        for (var i = 0; i < param_names.length; i++)
         {
             del_options += '<option>';
-            var subthing = thing[i];
-            console.log(subthing["name"]);
-            del_options += subthing["name"];
+            var entry_name = param_names[i];
+            console.log(entry_name["name"]);
+            del_options += entry_name["name"];
             del_options += '</option>';
         }
 
-        console.log("Thing: " + thing);
-        console.log("Subthing: " + subthing["name"]);
+        console.log("Parameter names: " + param_names);
+        console.log("Entry name: " + entry_name["name"]);
 
         $('#Delete_Data').append(
                 '<div class="large_text">Time Frame:</div>' +
@@ -44,14 +44,19 @@ function deleteTheStuff()
                 '<button type="button" onclick="filterData()">Filter</button><br/><br/>' +
                 '<div class="large_text">Please select the data entry from below:</div>' +
                 '<table id="deletion_space">' +
-                '<tr><th>Parameter Name</th><th>Date/Time</th><th>Value</th></tr>' +
-                '</table>'
+                '<tr><th>Date/Time</th><th>Name</th><th>Value</th><th>Author</th></tr>' +
+                '</table><br/>' +
+                '<button type="button" onclick="deleteData()">Delete</button><br/><br/>'
                 );
     });
 }
 
-function filterData()
-{
+function filterData() {
+    
+    //To store the string to append to the document
+    var htmlstring = "";
+    
+    //The entered/selected parameters are stored
     var $paramName = $('#delete_param').val();
     var $deleteStart = $('#delete_startdate').val();
     var $deleteEnd = $('#delete_enddate').val();
@@ -60,36 +65,59 @@ function filterData()
         parameter: '$paramName',
         startTime: '$deleteStart',
         endTime: '$deleteEnd'};
-    var filteredItems;
 
     /*
-     * Sample JSON response from our JSON file 
+     * Sample desired JSON response
      * data: [
      *  {
+     *      entryID : "1",
      *      name : "Soluble Reactive Phosphorus",
-     *      description : "Measure of phosphate...",
-     *      units : "ug P/L"
+     *      units : "ug P/L",
+     *      submittedBy : "Test User",
+     *      time : "2017-03-20T09:15:00",
+     *      value : "4.0"
      *  }
      * ]
      */
-    post("AdminServlet", filterRequest, function (resp) {
-        var items = JSON.parse(resp)["data"];
-        filteredItems = $(items).filter(function (index) {
-            return items[index].name === $paramName;
-        });
-        console.log('Stringified' + JSON.stringify(filteredItems));
+    post("AdminServlet", filterRequest, function(resp) {
+        var data = JSON.parse(resp)["data"];
+        for(var i = 0; i < data.length; i++)
+        {
+            var item = data[i];
+            htmlstring += '<tr id = deletion_row class = datadeletion>';
+            htmlstring += '<td><input type="text" name="data_time" id="time" value=' + item["time"] + '></td>';
+            htmlstring += '<td><input type="text" name="data_name" id="name" value=' + item["name"] + '></td>';
+            htmlstring += '<td><input type="text" name="data_value" id="value" value=' + item["value"] + '></td>';
+            htmlstring += '<td><input type="text" name="data_author" id="author" value=' + item["submittedBy"] + '></td>';
+            htmlstring += '<td><input type="checkbox" name="data_select" id="checkbox" value=' + item["entryID"] + '></td>';
+            htmlstring += '</tr>';
+            htmlstring += '<br/>';
+        }
+        
+        console.log('Stringified' + JSON.stringify(items));
     });
-    debugger
-    var filteredArray = JSON.parse(filteredItems);
-    filteredArray.forEach(function(item){
     
-        $('#deletion_space').append(
-                '<tr id = deletion_row class = datadeletion>'
-                + '<td><input type="text" name="data_name" id = "date" value=' + item["name"] + '></td>'
-                + '<td><input type="time" name="data_time" id = "time" value=10:15:30></td>'
-                + '<td><input type="text" name="value" id = "value" value=3.0></td>'
-                + '</tr>');
+    $('#deletion_space').append(htmlstring);
+}
 
-        console.log('Filtered Data: ' + $paramName + ' from ' + $deleteStart + ' to ' + $deleteEnd);
+function deleteData() {
+    
+    //TODO loop through listed entries, pass entryID of each selected
+    //entry to a POST that calls the DatabaseManager method manualDeletion
+    alert("Beep boop bop it's gonna happen!");
+    
+    var $idList;// = Array? of IDs
+    
+    var deleteRequest = {
+        action : "RemoveData",
+        idList : $idList
+    };
+    
+    //Not much needed in terms of feedback, except a confirmation
+    //before firing off the request for sure
+    post("AdminServlet", deleteRequest, function(resp) {
+        alert("Successful deletion of data");
     });
+    
+    alert("It happened. Beep beep boop.");
 }
