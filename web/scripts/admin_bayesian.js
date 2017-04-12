c846e8095a71fdb6a1fbb3af4b5caea2a4b96cd1
Brandon Meglathery <bsm29471@huskies.bloomu.edu>
2017-04-11 21:14:48 -0400

    Added a Bayesian tab
    woooow.

---
+++ web/scripts/admin_bayesian.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function fillBayesianContent()
+{
+    $('#Bayesian').append(
+            '<div class="large_text">THIS IS GONNA BE THE THING</div>'
+            + '<button type = "button" onclick="bayesianButton()">Bayesian GO</button>'
+            );
+}
